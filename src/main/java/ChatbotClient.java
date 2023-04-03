7d5ab2d2f09de9217791a7093ad1207b6d0201f3
junior <juninrody@gmail.com>
2023-04-02 22:36:39 -0300

    prompt da redação elaborado

--- src/main/java/ChatbotClient.java
+++ src/main/java/ChatbotClient.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -7,22 +9,49 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ChatbotClient {
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "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";
     private static final String MODEL_ID = "gpt-3.5-turbo";
+    private static final double TEMP_ID = 0.7;
+
 
     public static void main(String[] args) {
+
+        StringBuilder texto = new StringBuilder();
+        File meuArquivo = new File("src/main/java/arquivo.txt");
+        try {
+            Scanner scanner = new Scanner(meuArquivo);
+            while (scanner.hasNextLine()){
+                texto.append(scanner.nextLine());
+            }
+            scanner.close();
+        }catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
         HttpClient client = HttpClient.newHttpClient();
 
         List<ChatMessage> messages = new ArrayList<>();
 /*        messages.add(new ChatMessage("system", "You are a helpful assistant."));
         messages.add(new ChatMessage("user", "Who won the world series in 2020?"));
         messages.add(new ChatMessage("assistant", "The Los Angeles Dodgers won the World Series in 2020."));*/
-        messages.add(new ChatMessage("user", "oque é a lua?"));
+        messages.add(new ChatMessage("user", "analise a redação a seguir " +
+                "nos seguintes aspectos:" +
+                " estrutura, sequência lógica, parágrafo," +
+                " desenvolvimento do texto, " +
+                "o conteúdo " +
+                "A coesão lexical " +
+                "As conjunções e locuções conjuntivas e a " +
+                "ortografia." +
+                "No final de uma nota de 0 a 10" +
+                "Redação:\s" +
+                texto));
 
-        String requestBody = "{\"model\": \"" + MODEL_ID + "\", \"messages\": "
-                + messagesToJson(messages) + "}";
+        String requestBody = "{\"model\": \"" + MODEL_ID
+                + "\", \"messages\": " + messagesToJson(messages) + ","
+                +"\"temperature\":"+TEMP_ID+"}";
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(createUri())
@@ -60,6 +89,7 @@ public class ChatbotClient {
     private static class ChatMessage {
         private String role;
         private String content;
+
 
         public ChatMessage(String role, String content) {
             this.role = role;
